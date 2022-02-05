7406558ca9a54cf30483f29eaa6e5e29dcc6a63c
Anoop Naravaram <anoop901@gmail.com>
2022-02-05 14:38:57 -0800

    re-introduce form validation

--- src/app/guess-form/guess-form.component.ts
+++ src/app/guess-form/guess-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-guess-form',
@@ -9,9 +9,18 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class GuessFormComponent implements OnInit {
   GUESS_LENGTH = 5;
   guessFormGroup = new FormGroup({
-    word: new FormControl(''),
+    word: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(5),
+    ]),
   });
   @Output() onGuess = new EventEmitter<string>();
+
+  onSubmit() {
+    this.onGuess.emit(this.guessFormGroup.value.word.toLowerCase());
+    this.guessFormGroup.reset();
+  }
 
   constructor() {}
 
